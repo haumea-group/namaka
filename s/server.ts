e4a98dd443bbc85f02d95a947ba6c6f063a1f2ca
Chase Moskal <chasemoskal@gmail.com>
2022-04-29 05:13:22 -0700

    improve: node server port input

--- s/server.ts
+++ s/server.ts
@@ -6,10 +6,15 @@ import {nodeServer} from "renraku/x/http/node-server.js"
 import {makeApi} from "./api/api.js"
 import {databaseShape} from "./api/types/schema.js"
 
+const [,,input = "8000"] = process.argv
+const port = parseInt(input) ?? 8000
+
 nodeServer({
 	exposeErrors: false,
 	maxPayloadSize: renraku.megabytes(1),
 	api: await makeApi({
 		database: dbmage.memory({shape: databaseShape}),
 	}),
-}).listen(8000)
+}).listen(port)
+
+console.log(`🚀 namaka listening on port ${port}`)
