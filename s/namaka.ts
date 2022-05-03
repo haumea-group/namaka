f9aa4e65ccf293a61bd5accd25a88e7530ac7500
Chase Moskal <chasemoskal@gmail.com>
2022-05-03 06:56:24 -0700

    separate: frontend and backend index entrypoints
     - close #1

--- s/namaka.ts
+++ s/namaka.ts
@@ -1,8 +1,5 @@
 
-export * from "./api/api.js"
-export * from "./api/types/auth.js"
-export * from "./api/types/remote.js"
-export * from "./api/types/schema.js"
+export * from "./common-index.js"
 
 export * from "./frontend/framework/theme-components.js"
 export * from "./frontend/framework/mixins/mixin-styles.js"
@@ -11,6 +8,8 @@ export * from "./frontend/framework/mixins/mixin-standard.js"
 export * from "./frontend/framework/mixins/mixin-subscriptions.js"
 
 export * from "./frontend/models/prepare-models.js"
+
+export * from "./frontend/components/prepare-components.js"
 
 export * from "./frontend/startup/setup-mock-user.js"
 export * from "./frontend/startup/mock-api-connection.js"
