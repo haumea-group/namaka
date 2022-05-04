2f3bf180929af5cbc8503c0169edbd116966bf9a
joshuaolusayo <oyelekeoluwasayo@gmail.com>
2022-05-04 14:55:48 +0100

    updated the changes with the indentation. Also removed the font

--- s/frontend/components/reviews/empty-review/index.ts
+++ s/frontend/components/reviews/empty-review/index.ts
@@ -5,22 +5,22 @@ import namakaEmptyReviewCss from "./index.css.js"
 
 @mixinStyles(namakaEmptyReviewCss)
 export class NamakaEmptyReview extends LitElement {
-    render() {
-        return html`
-            <div class="empty-review">
-                <div class="heading">
-                    <span class="bold">Reviews</span>
-                    <select name="tag" id="tag">
-                        <option value="mostRecent">Most Recent</option>
-                        <option value="all">All</option>
-                    </select>
-                </div>
+	render() {
+		return html`
+			<div class="empty-review">
+				<div class="heading">
+					<span class="bold">Reviews</span>
+					<select name="tag" id="tag">
+						<option value="mostRecent">Most Recent</option>
+						<option value="all">All</option>
+					</select>
+				</div>
 
-                <div class="box">
-                    ${noDataSvg}
-                    <p class="gray">You currently don&apos;t have any reviews here. Keep trading and get reviews from other users.</p>
-                </div>
-            </div>
+				<div class="box">
+					${noDataSvg}
+					<p class="gray">You currently don&apos;t have any reviews here. Keep trading and get reviews from other users.</p>
+				</div>
+			</div>
         `
     }
 }
--- s/frontend/components/reviews/horizontal-five-star-review/index.ts
+++ s/frontend/components/reviews/horizontal-five-star-review/index.ts
@@ -4,48 +4,48 @@ import namakaHorizontalReviewCss from "./index.css.js"
 
 @mixinStyles(namakaHorizontalReviewCss)
 export class NamakaHorizontalReview extends LitElement {
-    render() {
-        return html`
-            <div class="horizontal-review">
-                <div class="grid">
-                    <div class="flex">
-                        <span class="gray">5 star</span>
-                        <div class="line">
-                           <div class="fill"></div>
-                        </div>
-                    </div>
-                    <div class="flex">
-                        <span class="gray">4 star</span>
-                        <div class="line">
-                           <div class="fill w-4"></div>
-                        </div>
-                    </div>
-                    <div class="flex">
-                        <span class="gray">3 star</span>
-                        <div class="line">
-                           <div class="fill w-3"></div>
-                        </div>
-                    </div>
-                    <div class="flex">
-                        <span class="gray">2 star</span>
-                        <div class="line">
-                           <div class="fill w-2"></div>
-                        </div>
-                    </div>
-                    <div class="flex">
-                        <span class="gray">1 star</span>
-                        <div class="line">
-                           <div class="fill w-1"></div>
-                        </div>
-                    </div>
-                    <div class="flex">
-                        <span class="gray">No star</span>
-                        <div class="line">
-                           <div class="fill w-0"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        `
-    }
+	render() {
+		return html`
+			<div class="horizontal-review">
+				<div class="grid">
+					<div class="flex">
+						<span class="gray">5 star</span>
+						<div class="line">
+						   <div class="fill"></div>
+						</div>
+					</div>
+					<div class="flex">
+						<span class="gray">4 star</span>
+						<div class="line">
+						   <div class="fill w-4"></div>
+						</div>
+					</div>
+					<div class="flex">
+						<span class="gray">3 star</span>
+						<div class="line">
+						   <div class="fill w-3"></div>
+						</div>
+					</div>
+					<div class="flex">
+						<span class="gray">2 star</span>
+						<div class="line">
+						   <div class="fill w-2"></div>
+						</div>
+					</div>
+					<div class="flex">
+						<span class="gray">1 star</span>
+						<div class="line">
+						   <div class="fill w-1"></div>
+						</div>
+					</div>
+					<div class="flex">
+						<span class="gray">No star</span>
+						<div class="line">
+						   <div class="fill w-0"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		`
+	}
 }
