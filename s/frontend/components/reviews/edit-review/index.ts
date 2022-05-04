d7681c7f08bf514181af46fcc8ea8ac6f1389d2b
joshuaolusayo <oyelekeoluwasayo@gmail.com>
2022-05-04 14:55:48 +0100

    updated the changes with the indentation. Also removed the font

--- s/frontend/components/reviews/edit-review/index.ts
+++ s/frontend/components/reviews/edit-review/index.ts
@@ -6,35 +6,35 @@ import namakaEditReviewCss from "./index.css.js"
 
 @mixinStyles(namakaEditReviewCss)
 export class NamakaEditReview extends LitElement {
-    render() {
-        return html`
-            <div class="edit-review">
-                <div class="box1">
-                    <div class="heading">
-                        <span class="title">Edit Review</span>
-                        <div class="close">${closeIconSvg}</div>
-                    </div>
-                    <p class="gray">Share your experience with the ValorExchange community, to help make better decisions</p>
-                </div>
-                <div class="share">
-                    <div class="group">
-                        <div>
-                            <p>Share Review?</p>
-                            <p>Please choose a review to rate this particular user.</p>
-                        </div>
-                        <div class="flex">
-                            ${starSvg}
-                            ${starSvg}
-                            ${starSvg}
-                            ${starSvg}
-                            ${starSvg}
-                        </div>
-                    </div>
-                    <div class="edit-feedback">
-                        <p><span class="black">Edit your feedback to this user</span> <span class="gray">(This will be made public)</span></p>
-                    </div>
-                </div>
-            </div>
+	render() {
+		return html`
+			<div class="edit-review">
+				<div class="box1">
+					<div class="heading">
+						<span class="title">Edit Review</span>
+						<div class="close">${closeIconSvg}</div>
+					</div>
+					<p class="gray">Share your experience with the ValorExchange community, to help make better decisions</p>
+				</div>
+				<div class="share">
+					<div class="group">
+						<div>
+							<p>Share Review?</p>
+							<p>Please choose a review to rate this particular user.</p>
+						</div>
+						<div class="flex">
+							${starSvg}
+							${starSvg}
+							${starSvg}
+							${starSvg}
+							${starSvg}
+						</div>
+					</div>
+					<div class="edit-feedback">
+						<p><span class="black">Edit your feedback to this user</span> <span class="gray">(This will be made public)</span></p>
+					</div>
+				</div>
+			</div>
         `
     }
 }
