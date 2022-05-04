c00aba7df3d7b085c8639cb74fc6e807997b088d
Przemek Gałęzki <przemekgg2002@gmail.com>
2022-05-04 17:48:16 +0200

    style: render-five-star-display

---
+++ s/frontend/components/common/five-stars/render-five-star-display.css.ts
@@ -0,0 +1,23 @@
+import { css } from 'lit';
+export default css`
+
+.five-star {
+	display:flex;
+	align-content:center;
+	align-items:center;
+
+}
+svg {
+	width: 20px;
+}
+
+.star-full svg path {
+	fill: yellow;
+	stroke: yellow;
+}
+
+.star-empty svg path {
+	fill: none;
+}
+
+`;
