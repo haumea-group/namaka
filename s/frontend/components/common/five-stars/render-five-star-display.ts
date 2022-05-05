2a10762939c29faa9b696659d277054fd11b9f27
Przemek Gałęzki <przemekgg2002@gmail.com>
2022-05-05 23:47:38 +0200

    add: on hover update number of stars

--- s/frontend/components/common/five-stars/render-five-star-display.ts
+++ s/frontend/components/common/five-stars/render-five-star-display.ts
@@ -36,11 +36,11 @@ export function renderFiveStarRating(state: FiveStarState,
 	
 	return html`
 		<div class="five-star">
-			<span @click=${() => updateRating(20)} class=${state.rating >= 20 ? "star-full" : "star-empty"}>${starSvg}</span>
-			<span @click=${() => updateRating(40)} class=${state.rating >= 40 ? "star-full" : "star-empty"}>${starSvg}</span>
-			<span @click=${() => updateRating(60)} class=${state.rating >= 60 ? "star-full" : "star-empty"}>${starSvg}</span>
-			<span @click=${() => updateRating(80)} class=${state.rating >= 80 ? "star-full" : "star-empty"}>${starSvg}</span>
-			<span @click=${() => updateRating(100)} class=${state.rating >= 100 ? "star-full" : "star-empty"}>${starSvg}</span>
+			<span @mouseover=${() => updateRating(20)} @click=${() => updateRating(20)} class=${state.rating >= 20 ? "star-full" : "star-empty"}>${starSvg}</span>
+			<span @mouseover=${() => updateRating(40)} @click=${() => updateRating(40)} class=${state.rating >= 40 ? "star-full" : "star-empty"}>${starSvg}</span>
+			<span @mouseover=${() => updateRating(60)} @click=${() => updateRating(60)} class=${state.rating >= 60 ? "star-full" : "star-empty"}>${starSvg}</span>
+			<span @mouseover=${() => updateRating(80)} @click=${() => updateRating(80)} class=${state.rating >= 80 ? "star-full" : "star-empty"}>${starSvg}</span>
+			<span @mouseover=${() => updateRating(100)} @click=${() => updateRating(100)} class=${state.rating >= 100 ? "star-full" : "star-empty"}>${starSvg}</span>
 		</div>
 	`;
 }
