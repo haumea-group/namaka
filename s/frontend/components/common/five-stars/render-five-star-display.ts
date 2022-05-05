ae7edda50f1b07ee82e35e8d377cd9da084727e1
Przemek Gałęzki <przemekgg2002@gmail.com>
2022-05-05 23:40:42 +0200

    add: state to re-render stars

--- s/frontend/components/common/five-stars/render-five-star-display.ts
+++ s/frontend/components/common/five-stars/render-five-star-display.ts
@@ -1,14 +1,46 @@
 import starSvg from '../../../icons/iconify/star.svg.js';
-import {html} from 'lit';
+import {html, render, TemplateResult} from 'lit';
+import './render-five-star-display.css.js';
 
-export function renderFiveStarRating(stars: number) {
+		export	function stateBuddy<T>(
+			element: HTMLElement,
+			initialState: T,
+			renderer: (
+				state: T,
+				setState: (s: T) => void,
+			) => TemplateResult,
+		) {
+			let state: T
+		function setState(newState: T) {
+				state = newState
+				render(renderer(state, setState), element)
+			}
+
+			setState(initialState)
+
+		return {
+				setState,
+				get state() {return state},
+			}
+		}
+interface FiveStarState {
+  rating: number
+}
+
+export function renderFiveStarRating(state: FiveStarState,
+    setState: (newState: FiveStarState) => void) {
+	
+	function updateRating(rating: number) {
+		return setState({rating})
+	}
+	
 	return html`
 		<div class="five-star">
-			<span class=${stars >= 1 ? "star-full" : "star-empty"}>${starSvg}</span>
-			<span class=${stars >= 2 ? "star-full" : "star-empty"}>${starSvg}</span>
-			<span class=${stars >= 3 ? "star-full" : "star-empty"}>${starSvg}</span>
-			<span class=${stars >= 4 ? "star-full" : "star-empty"}>${starSvg}</span>
-			<span class=${stars >= 5 ? "star-full" : "star-empty"}>${starSvg}</span>
+			<span @click=${() => updateRating(20)} class=${state.rating >= 20 ? "star-full" : "star-empty"}>${starSvg}</span>
+			<span @click=${() => updateRating(40)} class=${state.rating >= 40 ? "star-full" : "star-empty"}>${starSvg}</span>
+			<span @click=${() => updateRating(60)} class=${state.rating >= 60 ? "star-full" : "star-empty"}>${starSvg}</span>
+			<span @click=${() => updateRating(80)} class=${state.rating >= 80 ? "star-full" : "star-empty"}>${starSvg}</span>
+			<span @click=${() => updateRating(100)} class=${state.rating >= 100 ? "star-full" : "star-empty"}>${starSvg}</span>
 		</div>
 	`;
 }
