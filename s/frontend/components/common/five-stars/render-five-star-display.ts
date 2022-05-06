a88e7bcb3a4b88c09288ff979b6cb3ec868fbf08
Przemek Gałęzki <przemekgg2002@gmail.com>
2022-05-07 00:25:07 +0200

    add: stop hover stars on click

--- s/frontend/components/common/five-stars/render-five-star-display.ts
+++ s/frontend/components/common/five-stars/render-five-star-display.ts
@@ -1,46 +1,61 @@
 import starSvg from '../../../icons/iconify/star.svg.js';
-import {html, render, TemplateResult} from 'lit';
+import { html, render, TemplateResult } from 'lit';
+import { repeat } from 'lit-html/directives/repeat.js';
 import './render-five-star-display.css.js';
 
-		export	function stateBuddy<T>(
-			element: HTMLElement,
-			initialState: T,
-			renderer: (
-				state: T,
-				setState: (s: T) => void,
-			) => TemplateResult,
-		) {
-			let state: T
-		function setState(newState: T) {
-				state = newState
-				render(renderer(state, setState), element)
-			}
+export function stateBuddy<T>(
+	element: HTMLElement,
+	initialState: T,
+	renderer: (state: T, setState: (s: T) => void) => TemplateResult
+) {
+	let state: T;
+	function setState(newState: T) {
+		state = newState;
+		render(renderer(state, setState), element);
+	}
 
-			setState(initialState)
+	setState(initialState);
 
-		return {
-				setState,
-				get state() {return state},
-			}
-		}
+	return {
+		setState,
+		get state() {
+			return state;
+		},
+	};
+}
 interface FiveStarState {
-  rating: number
+	rating: number;
+	clicked: boolean | undefined;
 }
 
-export function renderFiveStarRating(state: FiveStarState,
-    setState: (newState: FiveStarState) => void) {
-	
-	function updateRating(rating: number) {
-		return setState({rating})
+export function renderFiveStarRating(
+	state: FiveStarState,
+	setState: (newState: FiveStarState) => void
+) {
+	function updateRating(rating: number, e?: Event) {
+		const clicked = state.clicked;
+		if (e?.type == 'click') {
+			let clicked = !state.clicked;
+			return setState({ rating, clicked });
+		} else {
+			return setState({ rating, clicked });
+		}
 	}
-	
+	function renderStar(rating: number) {
+		return html` <span
+			@mouseover=${() => {
+				if (!state.clicked) updateRating(rating);
+			}}
+			@click=${(e: Event) => updateRating(rating, e)}
+			class=${state.rating >= rating ? 'star-full' : 'star-empty'}
+			>${starSvg}</span
+		>`;
+	}
 	return html`
-		<div class="five-star">
-			<span @mouseover=${() => updateRating(20)} @click=${() => updateRating(20)} class=${state.rating >= 20 ? "star-full" : "star-empty"}>${starSvg}</span>
-			<span @mouseover=${() => updateRating(40)} @click=${() => updateRating(40)} class=${state.rating >= 40 ? "star-full" : "star-empty"}>${starSvg}</span>
-			<span @mouseover=${() => updateRating(60)} @click=${() => updateRating(60)} class=${state.rating >= 60 ? "star-full" : "star-empty"}>${starSvg}</span>
-			<span @mouseover=${() => updateRating(80)} @click=${() => updateRating(80)} class=${state.rating >= 80 ? "star-full" : "star-empty"}>${starSvg}</span>
-			<span @mouseover=${() => updateRating(100)} @click=${() => updateRating(100)} class=${state.rating >= 100 ? "star-full" : "star-empty"}>${starSvg}</span>
-		</div>
+		${renderStar(20)}
+		${renderStar(40)}
+		${renderStar(60)}
+		${renderStar(80)}
+		${renderStar(100)}
 	`;
 }
