79d3c0f229ffa5c7af6f6f0ff98a7c09db0bbf70
Chase Moskal <chasemoskal@gmail.com>
2022-05-15 11:33:52 -0700

    tweak: jsx declarations

--- s/jsx.ts
+++ s/jsx.ts
@@ -1,12 +1,12 @@
 
+import {NamakaModals} from "./frontend/components/modals/namaka-modals.js"
 import {NamakaBannedUsers} from "./frontend/components/banned-users/index.js"
 import {NamakaComments} from "./frontend/components/comments/namaka-comments.js"
-import {NamakaReview} from "./frontend/components/common/five-stars/namaka-review.js"
 import {NamakaDemoAuth} from "./frontend/components/demo-auth/namaka-demo-auth.js"
-import {NamakaModals} from "./frontend/components/modals/namaka-modals.js"
+import {NamakaReview} from "./frontend/components/common/five-stars/namaka-review.js"
 import {NamakaTextarea} from "./frontend/components/namaka-textarea/namaka-textarea.js"
 
-declare module JSX {
+export declare module JSX {
 	interface IntrinsicElements {
 		"namaka-banned-users": NamakaBannedUsers
 		"namaka-comments": NamakaComments
