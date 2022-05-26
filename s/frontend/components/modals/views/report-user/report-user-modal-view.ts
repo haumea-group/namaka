9dd2b7bbaec909c7ec8e2280bdb8dd14f8a9be15
Chase Moskal <chasemoskal@gmail.com>
2022-05-26 15:23:06 -0700

    tweak: report user modal

--- s/frontend/components/modals/views/report-user/report-user-modal-view.ts
+++ s/frontend/components/modals/views/report-user/report-user-modal-view.ts
@@ -17,20 +17,22 @@ export function reportUserModalView({
 		Username: ${comment.user.profile.nickname}
 		userId: ${comment.user.id}
 		commentId: ${comment.id}
-	`
+	`.split("\n").map(s => s.trim()).join("\n").trim()
 
 	modals.openModal({
 		closeOnBlanketClick: true,
 		renderContent: ({closeModal}) => html`
 			<div part="container" class="modalview report">
 
-				<slot name=report-user-lol-custom>Default Custom Text</slot>
-
 				<h2>${alertTriangleSvg} Report user</h2>
-				<p>Please send an email to the following address to submit your report against this user's comment.</p>
+				<slot name=report-user-explainer>
+					Please send an email to the following address to submit your report against this user's comment.
+				</slot>
 
-				<h2><slot name=support-email>example@example.com</slot></h2>
-				<p>Please include the following information in the email, so we can identify which comment and user.</p>
+				<h2><slot name=report-user-support-email>example@example.com</slot></h2>
+				<slot name=report-user-how-to-copy>
+					Please include the following information in the email, so we can identify which comment and user.
+				</slot>
 
 				<namaka-copy-to-clipboard
 					copyText=${copyText}
