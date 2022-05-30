673e50ac1dea7bcb79d5227849d45692d3df16cb
Paul_Aroo <timmypaul2006@gmail.com>
2022-05-30 17:24:25 +0100

    implement: howLongAgo utility function

--- s/toolbox/how-long-ago.ts
+++ s/toolbox/how-long-ago.ts
@@ -1,5 +1,23 @@
 
 export function howLongAgo(time: number) {
-	// TODO implement
-	return "awhile ago"
+	const elapsed = Math.floor((Date.now() - time)/1000)
+	if (Date.now() - time < 1001)
+		return null
+	else
+		return `${prettify(elapsed)} ago`
+}
+
+const prettify = (timeInSeconds: number) => {
+	if (timeInSeconds < 60) {
+		return `${timeInSeconds} second${timeInSeconds > 1 ? "s" : ""}`
+	} else if (timeInSeconds > 60 && timeInSeconds < 3600) {
+		const val = Math.floor(timeInSeconds / 60)
+		return`${val} minute${val>1 ? "s" : ""}`
+	} else if (timeInSeconds > 3600 && timeInSeconds < 86400) {
+		const val = Math.floor(timeInSeconds / 3600)
+		return`${val} hour${val>1 ? "s" : ""}`
+	} else {
+		const val = Math.floor(timeInSeconds / 86400)
+		return`${val} day${val>1 ? "s" : ""}`
+	}
 }
