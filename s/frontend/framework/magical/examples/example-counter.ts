a7fc778f6caed07fa25db33b149f9aa5fbf7b537
Chase Moskal <chasemoskal@gmail.com>
2022-06-05 01:00:59 -0700

    tweak: naming in example counter

--- s/frontend/framework/magical/examples/example-counter.ts
+++ s/frontend/framework/magical/examples/example-counter.ts
@@ -4,10 +4,10 @@ import {magical} from "../magical.js"
 
 export const ExampleCounter = magical(use => (start: number) => {
 	const [count, setCount] = use.state(start)
-	const handleClick = () => setCount(count + 1)
+	const increment = () => setCount(count + 1)
 
 	return html`
 		<p>count ${count}</p>
-		<button @click=${handleClick}>increment</button>
+		<button @click=${increment}>increment</button>
 	`
 })
